659702d66f482038b00b6f77f167e4234c8271d1
Milad Faridnia <MiladFaridnia@users.noreply.github.com>
2023-05-14 13:09:39 +0430

    Feat: Upgrade room database and fix some issues

--- app/src/main/java/com/faridnia/mystrava/db/Run.kt
+++ app/src/main/java/com/faridnia/mystrava/db/Run.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "running_table")
 data class Run(
-    val timeStamp: Long = 0L, //when the run was
-    val runDurationInMillis: Long = 0L,
-    val distanceInMeters: Int = 0,
-    val avgSpeedInKMH: Float = 0F,
-    val image: Bitmap? = null,
-    val caloriesBurned: Int = 0
+    var timeStamp: Long = 0L, //when the run was
+    var runDurationInMillis: Long = 0L,
+    var distanceInMeters: Int = 0,
+    var avgSpeedInKMH: Float = 0F,
+    var image: Bitmap? = null,
+    var caloriesBurned: Int = 0
 ) {
     @PrimaryKey(autoGenerate = true)
-    val id: Int? = null
+    var id: Int? = null
 }
