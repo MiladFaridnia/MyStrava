7d672c6175f18028e46638d0ff148a052d3e304d
Milad Faridnia <MiladFaridnia@users.noreply.github.com>
2023-06-18 18:28:21 +0330

    Feat: Add Foreground Service

--- app/src/main/java/com/faridnia/mystrava/other/Constants.kt
+++ app/src/main/java/com/faridnia/mystrava/other/Constants.kt
@@ -9,4 +9,8 @@ object Constants {
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
 
+    const val NOTIFICATION_CHANNEL_ID = "my_strava_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1
+    const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
 }
